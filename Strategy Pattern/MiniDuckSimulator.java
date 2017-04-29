7fbf255c8f0ea75a8d1886b81ff7cc042f754bfe
nayakan <nayakj9@gmail.com>
2017-04-28 18:44:45 -0700

    Update MiniDuckSimulator.java

--- Strategy Pattern/MiniDuckSimulator.java
+++ Strategy Pattern/MiniDuckSimulator.java
@@ -3,11 +3,11 @@ public class MiniDuckSimulator {
  
 	public static void main(String[] args) {
  
-		MallardDuck	mallard = new MallardDuck();
-		RubberDuck	rubberDuckie = new RubberDuck();
-		DecoyDuck	decoy = new DecoyDuck();
+		MallardDuck mallard = new MallardDuck();
+		RubberDuck rubberDuckie = new RubberDuck();
+		DecoyDuck decoy = new DecoyDuck();
  
-		ModelDuck	model = new ModelDuck();
+		ModelDuck model = new ModelDuck();
 
 		mallard.performQuack();
 		rubberDuckie.performQuack();
